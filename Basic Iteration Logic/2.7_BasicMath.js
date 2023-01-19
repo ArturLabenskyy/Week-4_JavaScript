52f32b7efcf5901f2b52b8c23eefd459c3ad1e60
ArturLabenskyy <dodik.dod@hotmail.com>
2023-01-19 15:27:36 +0200

    2.7

---
+++ Basic Iteration Logic/2.7_BasicMath.js
@@ -0,0 +1,28 @@
+// Ex2.7 - Basic Math
+// Your task is to create a function that does four basic .
+// The function should take three arguments - operation(string/char), value1(number),
+// value2(number).
+// The function should return result of numbers after applying the chosen operation.
+// Examples
+// basicOp('+', 4, 7) // Output: 11
+// basicOp('-', 15, 18) // Output: -3
+// basicOp('*', 5, 5) // Output: 25
+// basicOp('/', 49, 7) // Output: 7
+
+function basicOp(opr, val1, val2) {
+    switch (opr) {
+        case `+`:
+            return val1 + val2;
+        case `-`:
+            return val1 - val2;
+        case `*`:
+            return val1 * val2;
+        case `/`:
+            return val1 / val2;
+    }
+}
+
+console.log(basicOp("+", 4, 7));
+console.log(basicOp("-", 15, 18));
+console.log(basicOp("*", 5, 5));
+console.log(basicOp("/", 49, 7));
