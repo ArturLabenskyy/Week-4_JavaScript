55c4ae0f2699782447fc781121917323000c9f33
ArturLabenskyy <dodik.dod@hotmail.com>
2023-01-19 13:39:23 +0200

    1.1

--- Basic/1.1_Yes_or_No.js
+++ Basic/1.1_Yes_or_No.js
@@ -1,3 +1,7 @@
+// Ex1.1 - Yes or No
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No"
+// string for false.
+
 function yesOrNo(value) {
     return value ? `Yes` : `No`;
 }
