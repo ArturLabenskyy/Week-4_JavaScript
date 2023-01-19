7feaf66a0cc33fedf24fba7cb2fa44b0eff7ee8e
ArturLabenskyy <dodik.dod@hotmail.com>
2023-01-19 20:52:30 +0200

    4.2

--- 5_Basic Iteration Logic/5.7_shortestWords.js
+++ 5_Basic Iteration Logic/5.7_shortestWords.js
@@ -1,6 +1,9 @@
 // Ex5.7 - shortest words
 // Simple, given a string of words, return the length of the shortest word(s).
 // String will never be empty and you do not need to account for different data types.
-function shortestWord(str) {}
+function shortestWord(str) {
+    console.log(str.slice(0, str.indexOf(` `)));
+    console.log(str.slice(str.indexOf(` `), str.indexOf(` `)));
+}
 
 console.log(shortestWord(`The shortest word issssssss`));
