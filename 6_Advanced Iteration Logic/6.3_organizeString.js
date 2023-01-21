3af63f0c246508beb2339a33b3dee36cf9fe23bf
ArturLabenskyy <dodik.dod@hotmail.com>
2023-01-21 12:04:07 +0200

    6.3

--- 6_Advanced Iteration Logic/6.3_organizeString.js
+++ 6_Advanced Iteration Logic/6.3_organizeString.js
@@ -1,7 +1,21 @@
 function organizeString(str1, str2) {
     let organize = str1.concat(``, str2);
     organize = organize.split(``);
-    console.log(organize);
+    organize = organize.sort((a, b) => a.charCodeAt(a) - b.charCodeAt(b));
+    let str = [];
+    for (let i = 0; i < organize.length; i++) {
+        if (i === organize.length - 1 && organize[i] != organize[i - 1]) {
+            str.push(organize[i]);
+            continue;
+        }
+        if (organize[i] != organize[i + 1]) {
+            str.push(organize[i]);
+        }
+    }
+    return str.join(``);
 }
 
 console.log(organizeString(`xyaabbbccccdefww`, `xxxxyyyyabklmopq`));
+console.log(
+    organizeString(`abcdefghijklmnopqrstuvwxyz`, `abcdefghijklmnopqrstuvwxyz`)
+);
